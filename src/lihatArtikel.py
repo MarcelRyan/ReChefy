31fd0d77c774fc4a371dec2b92a2668b2d63025b
Raylouis <sebastianraynard@gmail.com>
2023-04-15 17:20:43 +0700

    feat : lihatArtikel styling scrollArea and fix some bug

--- src/lihatArtikel.py
+++ src/lihatArtikel.py
@@ -10,10 +10,23 @@ class lihatArtikel(QMainWindow) :
         self.setStyleSheet("background-color: #FDE7BD;")
         self.show()
 
+        self.scroll_bar = self.textEdit.verticalScrollBar()
+
+        self.scrollArea.verticalScrollBar().setStyleSheet("QScrollBar:vertical {background-color: #FDE7BD; border: none; border-radius: 15px; width: 8px; margin: 0px 0px 0px 0px;}\
+            QScrollBar::handle:vertical {background-color: #EE9C20;border-radius: 15px; min-height: 20px;}\
+            QScrollBar::add-line:vertical {border: none; background: none;}\
+            QScrollBar::sub-line:vertical {border: none; background: none;}")
+    
+        self.scroll_bar.setStyleSheet("QScrollBar:vertical {background-color: #FDE7BD; border: none; border-radius: 15px; width: 8px; margin: 0px 0px 0px 0px;}\
+            QScrollBar::handle:vertical {background-color: #EE9C20;border-radius: 15px; min-height: 20px;}\
+            QScrollBar::add-line:vertical {border: none; background: none;}\
+            QScrollBar::sub-line:vertical {border: none; background: none;}")
+
+
         self.file = r".\src\database\rechefy.db"
         self.connection = database_func.connectToDatabase(self.file)
-        self.artikel = database_func.getArtikel(self.connection, 1)
-        self.artikelFoto = database_func.resepBlobToImage(self.connection, 1)
+        self.artikel = database_func.getArtikel(self.connection,1)
+        self.artikelFoto = database_func.artikelBlobToImage(self.connection, 1)
 
         self.fotoArtikel.setPixmap(QtGui.QPixmap(self.artikelFoto))
         self.namaArtikel.setText(str(self.artikel[2]))
