71ff9cccc5945b6cf28b7768169cfe718d98f664
kenndave <dave.bahana@gmail.com>
2023-04-15 23:47:14 +0700

    add FormEditResep

--- src/editResep.py
+++ src/editResep.py
@@ -5,21 +5,18 @@ from PyQt5 import uic, QtWidgets
 from database import database_func
 import sys
 
-class FormAddResep(QtWidgets.QMainWindow):
+class FormEditResep(QtWidgets.QMainWindow):
     def __init__(self):
-        super(FormAddResep, self).__init__()
-        uic.loadUi("src/addResep.ui", self)
-        #self.parent = parent
+        super(FormEditResep, self).__init__()
+        uic.loadUi("src/editResep.ui", self)
+        self.idResep = 1
         self.setFixedHeight(850)
         self.setFixedWidth(1200)
-        self.saveButton_resep.clicked.connect(self.inputValidation)
         self.inputGambar_resep.setIconSize(QSize(113, 98))
-        self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #EEC120; border-radius: 25px;}")
-        self.inputGambar_resep.setIcon(QIcon(QPixmap("images/icon/pilihFoto.png")))
+        self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #EEC120; border: none;}")
         self.inputGambar_resep.clicked.connect(self.selectPicture)
+        self.saveButton_resep.clicked.connect(self.inputValidation)
 
-        self.addAlat_button.clicked.connect(self.addAlat)
-        self.addBahan_button.clicked.connect(self.addBahan)
         self.addAlat_button.setFixedSize(30, 30)
         self.addBahan_button.setFixedSize(30, 30)
         self.addAlat_button.setIcon(QIcon(QPixmap("images/icon/addIcon.png")))
@@ -39,10 +36,11 @@ class FormAddResep(QtWidgets.QMainWindow):
                                                 QScrollBar::handle:vertical {background-color: #EE9C20;border-radius: 15px; min-height: 20px;}\
                                                 QScrollBar::add-line:vertical {border: none; background: none;}\
                                                 QScrollBar::sub-line:vertical {border: none; background: none;}")
+        self.scrollAlat.setStyleSheet("QScrollBar:vertical { width: 15px; }")
+        self.scrollBahan.setStyleSheet("QScrollBar:vertical { width: 15px; }")
+
         self.verticalAlat = QtWidgets.QVBoxLayout()
         self.verticalBahan = QtWidgets.QVBoxLayout()
-        self.filePath = ""
-        self.verticalBahan.setContentsMargins(0, 0, 10, 0)
         self.listAlat = []
         self.listBahan = []
         self.counterAlat = 1
@@ -50,25 +48,46 @@ class FormAddResep(QtWidgets.QMainWindow):
         self.amountAlat = 0
         self.amountBahan = 0
 
-
-        # Read database
-        self.file = r".\src\database\rechefy.db"
+        ## EDIT RESEP DATABASE
+        self.file = r"src\database\rechefy.db"
         self.connection = database_func.connectToDatabase(self.file)
-        database_func.initializeTable(self.connection)
+        self.dataResep = database_func.getResep(self.connection, self.idResep)
+        #foto = x[0][1]
+        nama = self.dataResep[0][2]
+        deskripsi = self.dataResep[0][3]
+        langkah = self.dataResep[0][4]
+        self.inputJudul_resep.setText(nama)
+        self.inputDeskripsi_resep.setText(deskripsi)
+        self.inputLangkahMemasak_resep.setText(langkah)
+        self.filePath = database_func.resepBlobToImage(self.connection, 1)
+        self.inputGambar_resep.setIcon(QIcon(QPixmap(self.filePath)))
+        self.inputGambar_resep.setIconSize(QSize(self.inputGambar_resep.width(), self.inputGambar_resep.height()))
+        self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #FFF6E5; border: none;}")
+
         self.allAlat = database_func.getAlat(self.connection)
         self.allBahan = database_func.getBahan(self.connection)
         self.satuanBahan = database_func.getSatuanKuantitasBahan(self.connection)
+        y = database_func.getAlatResep(self.connection, 1)
+        z = database_func.getBahanResep(self.connection, 1)
+        for i in y:
+            self.addAlat(i[1])
+        for i in z:
+            self.addBahan(i[1], i[2], i[3])
+        self.addAlat_button.clicked.connect(lambda: self.addAlat(self.allAlat[0][1]))
+        self.addBahan_button.clicked.connect(lambda: self.addBahan(self.allBahan[0][1], 0, self.satuanBahan[0][0]))
 
-    def addAlat(self):
+        
+    def addAlat(self, opsi):
         horizontal_layout = QtWidgets.QHBoxLayout()
         horizontal_layout.setObjectName("Alat_"+str(self.counterAlat))
 
         dropdown = QtWidgets.QComboBox()
         for i in self.allAlat:
             dropdown.addItem(i[1])
+        dropdown.setCurrentIndex(dropdown.findText(opsi))
         dropdown.view().setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         dropdown.setObjectName("DropdownAlat_"+str(self.counterAlat))
-        dropdown.setFixedSize(132, 30)
+        dropdown.setFixedSize(133, 30)
         dropdown.setStyleSheet("QComboBox{background-color: #F7EAD3; border: none; border-radius: 10px;} QComboBox::down-arrow {image: url(images/icon/down_arrow.png); height: 30px;} ")
         horizontal_layout.addWidget(dropdown)
 
@@ -88,14 +107,13 @@ class FormAddResep(QtWidgets.QMainWindow):
         self.amountAlat += 1
         
     
-    def addBahan(self):
+    def addBahan(self, opsi, jumlah, satuan):
         horizontal_layout = QtWidgets.QHBoxLayout()
-        horizontal_layout.setObjectName("Bahan_"+str(self.counterAlat))
-        horizontal_layout.setSpacing(3)
 
         amount = QtWidgets.QDoubleSpinBox()
         amount.setObjectName("Jumlah_"+str(self.counterBahan))
         amount.setRange(0.1, 100000.0)
+        amount.setValue(jumlah)
         amount.setFixedSize(45, 30)
         amount.setStyleSheet("QSpinBox{background-color: #F7EAD3; border-radius: 10px;}QSpinBox::up-button{image: url(images/icon/up_arrow.png); width: 7px;} QSpinBox::down-button{image: url(images//icon/down_arrow.png); width: 7px;}")
         horizontal_layout.addWidget(amount)
@@ -103,8 +121,9 @@ class FormAddResep(QtWidgets.QMainWindow):
         unit = QtWidgets.QComboBox()
         for i in self.satuanBahan:
             unit.addItem(i[0])
-        unit.addItems(['kg', 'object'])
         unit.setObjectName("Satuan_"+str(self.counterBahan))
+        #if satuan != "none":
+        unit.setCurrentIndex(unit.findText(satuan))
         unit.setFixedSize(72, 30)
         unit.setStyleSheet("QComboBox{background-color: #F7EAD3; border: none; border-radius: 10px;} QComboBox::down-arrow {image: url(images/icon/down_arrow.png); height: 5px; width: 5px}")
         horizontal_layout.addWidget(unit)
@@ -112,6 +131,7 @@ class FormAddResep(QtWidgets.QMainWindow):
         dropdown = QtWidgets.QComboBox()
         for i in self.allBahan:
             dropdown.addItem(i[1])
+        dropdown.setCurrentIndex(dropdown.findText(opsi))
         dropdown.setObjectName("DropdownBahan_"+str(self.counterBahan))
         dropdown.setFixedSize(125, 30)
         dropdown.setStyleSheet("QComboBox{background-color: #F7EAD3; border: none; border-radius: 10px;} QComboBox::down-arrow {image: url(images/icon/down_arrow.png); height: 30px;}")
@@ -140,15 +160,13 @@ class FormAddResep(QtWidgets.QMainWindow):
     def selectPicture(self):
         self.filePath, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.png *.jpg *.bmp)")
         if self.filePath:
-            print(self.filePath)
             self.inputGambar_resep.setIcon(QIcon(QPixmap(self.filePath)))
             self.inputGambar_resep.setIconSize(QSize(self.inputGambar_resep.width(), self.inputGambar_resep.height()))
             self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #FFF6E5; border: none;}")
         else:
-            print(self.filePath)
             self.inputGambar_resep.setIcon(QIcon(QPixmap("images/icon/pilihFoto.png")))
             self.inputGambar_resep.setIconSize(QSize(113, 98))
-            self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #EEC120; border-radius: 25px;}")
+            self.inputGambar_resep.setStyleSheet("QPushButton{background-color: #EEC120; border: none;}")
 
     def deleteAlat(self, layout):
         # Get the layout item that contains the given layout
@@ -183,8 +201,6 @@ class FormAddResep(QtWidgets.QMainWindow):
         self.listBahan.remove(layout.id)
         self.amountBahan -= 1
     
-    # def validasiResep(self):
-    #     if ()
     def alatValidation(self):
         alat = set()
         for count in self.listAlat:
@@ -219,34 +235,29 @@ class FormAddResep(QtWidgets.QMainWindow):
                 print("Terdapat alat yang sama")
             if not self.bahanValidation():
                 print("Terdapat bahan yang sama")
-
         else:
-            self.addResep()
-            # penambahan warning
-    def addResep(self):
+            print("Berhasil edit")
+            self.editResep()
+        # penambahan warning
+    def editResep(self):
         # Jika tervalidasi, lakukan add resep.
-        # def addResep(connection, nama_masakan, deskripsi_masakan, gambar_masakan, langkah_memasak, isDefault):
-        database_func.addResep(self.connection, self.inputJudul_resep.toPlainText(), self.inputDeskripsi_resep.toPlainText(), database_func.imageToBlob(self.filePath), self.inputLangkahMemasak_resep.toPlainText(), 1)
-        self.resepID = database_func.getLastIdResep(self.connection)
-        
+        database_func.editResep(self.connection, self.idResep, database_func.imageToBlob(self.filePath), self.inputJudul_resep.toPlainText(), self.inputDeskripsi_resep.toPlainText(), self.inputLangkahMemasak_resep.toPlainText())
+        database_func.deleteAlatResep(self.connection, self.idResep)
+        database_func.deleteBahanResep(self.connection, self.idResep)
         for count in self.listAlat:
             alat = self.findChild(QComboBox, f'DropdownAlat_{count}')
             idAlat = database_func.getIdAlat(self.connection, alat.currentText())
-            database_func.addAlatResep(self.connection, self.resepID, idAlat)
-            #addAlatResep(connection, id_resep, id_alat)
+            database_func.addAlatResep(self.connection, self.idResep, idAlat)
         
         for count in self.listBahan:
-            print(count)
             bahan = self.findChild(QComboBox, f'DropdownBahan_{count}')
             jumlahBahan = self.findChild(QDoubleSpinBox, f'Jumlah_{count}')
-            print(jumlahBahan.value())
             satuanBahan = self.findChild(QComboBox, f'Satuan_{count}')
             idBahan = database_func.getIdBahan(self.connection, bahan.currentText())
-            #addBahanResep(connection, id_resep, id_bahan, kuantitas_bahan, satuan_kuantitas_bahan)
-            database_func.addBahanResep(self.connection, self.resepID, idBahan, jumlahBahan.value(), satuanBahan.currentText())
+            database_func.addBahanResep(self.connection, self.idResep, idBahan, jumlahBahan.value(), satuanBahan.currentText())
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = FormAddResep()
+    MainWindow = FormEditResep()
     MainWindow.show()
     sys.exit(app.exec_())
