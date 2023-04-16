18ebe318e2d288376d81735c5370b03064ca1f58
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-16 18:54:09 +0700

    testing ci cd

--- tests/test_database_func.py
+++ tests/test_database_func.py
@@ -14,3 +14,8 @@ def test_addAlat():
     db_connector = Mock()
     databaseFunc.initializeTable(db_connector)
     databaseFunc.addAlat(db_connector, "tes")
+
+def test_addBahan():
+    db_connector = Mock()
+    databaseFunc.initializeTable(db_connector)
+    databaseFunc.addBahan(db_connector, "tes")
