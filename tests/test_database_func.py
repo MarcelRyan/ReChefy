22a63d40680feb9bdb571536e66c95709831657f
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-16 22:50:34 +0700

    change unit testing and ci cd

--- tests/test_database_func.py
+++ tests/test_database_func.py
@@ -1,20 +1,46 @@
 import sys
-from unittest.mock import Mock
+import unittest
 from src.database.databaseFunc import *
 
-def test_initializeTable():
-    db_connector = Mock()
-    initializeTable(db_connector)
+class ModuleTesting(unittest.TestCase):
 
-def test_connectToDatabase():
-    assert connectToDatabase("test.db") == None
+    def testGetIDResep(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        self.assertEqual(getIdResep(self.connection, "Tempe Mendoan"), 14)
+    
+    def testGetIDAlat(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        self.assertEqual(getIdAlat(self.connection, "Loyang"), 26)
+    
+    def testGetIDBahan(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        self.assertEqual(getIdBahan(self.connection, "Garam"), 9)
+    
+    def testDeleteResep(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        deleteResep(self.connection, 10)
+        self.assertEqual(getResep(self.connection, 10), [])
 
-def test_addAlat():
-    db_connector = Mock()
-    initializeTable(db_connector)
-    addAlat(db_connector, "tes")
-
-def test_addBahan():
-    db_connector = Mock()
-    initializeTable(db_connector)
-    addBahan(db_connector, "tes")
+    def testDeleteAlatResep(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        deleteAlatResep(self.connection, 10)
+        self.assertEqual(getAlatResep(self.connection, 10), [])
+    
+    def testDeleteBahanResep(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        deleteBahanResep(self.connection, 10)
+        self.assertEqual(getBahanResep(self.connection, 10), [])
+    
+    def testGetKomentar(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        self.assertEqual(getKomentar(self.connection, 10), [])
+    
+    def testAddAlat(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        # addAlat(self.connection, "tesgan") Udah di tambahin di unit testing pertama
+        self.assertEqual(getIdAlat(self.connection, "tesgan"), 27)
+    
+    def testAddBahan(self):
+        self.connection = connectToDatabase(r"./tests/test.db")
+        # addBahan(self.connection, "tesgan") Udah ditambahin di unit testing pertama
+        self.assertEqual(getIdBahan(self.connection, "tesgan"), 91)
