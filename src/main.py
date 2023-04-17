ece162e897fd3d901669d7f90039eda6ef6924bc
Raylouis <sebastianraynard@gmail.com>
2023-04-18 02:23:37 +0700

    feat : add main.py

--- src/main.py
+++ src/main.py
@@ -1,25 +1,9 @@
 from PyQt5.QtWidgets import *
-from PyQt5 import uic, QtWidgets
 import sys
-import menu
-import welcomePage
-
-class Main(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.pages = QtWidgets.QStackedWidget()
-        self.pages.setFixedHeight(850)
-        self.pages.setFixedWidth(1200)
-        self.setCentralWidget(self.pages)
-
-        self.WelcomePage = welcomePage.WelcomePage(self)
-        self.pages.addWidget(self.WelcomePage)
-        
-        self.Menu = menu.Menu(self)
-        self.pages.addWidget(self.Menu)
+import controller
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main = Main()
+    main = controller.Controller()
     main.show()
     sys.exit(app.exec_())
